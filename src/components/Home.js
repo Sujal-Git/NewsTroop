b7e926f9b459b9ea5293d8482160455fa1d6fd78
Sujal <sujalvishwakarma93@gmail.com>
2023-07-23 18:08:08 +0530

    Initialization of Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import './Home.css'
+import News from './News'
 
 export default function Home() {
     return(
         <>
         <h1>Home Page</h1>
+        <News/>
+        <News/>
+        <News/>
+        <News/>
+        <News/>
+
         </>
     )
 }
