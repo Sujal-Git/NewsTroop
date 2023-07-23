4152ee9a60bbce645a86ccb9bd582252c53181e9
Sujal <sujalvishwakarma93@gmail.com>
2023-07-23 11:39:07 +0530

    Added News component

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './News.css'
+
+export default function News() {
+  return (
+      <>
+      <div className="main">
+        <h3>This Is Headline</h3>
+        <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong></p>
+      </div>
+      </>
+  )
+}
