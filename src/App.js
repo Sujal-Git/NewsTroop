2ab42cd3a5c9b1fceea2802da8362aab6161ae46
Sujal <sujalvishwakarma93@gmail.com>
2023-07-23 10:21:15 +0530

    Fixed Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
-
+import {Link,Routes,Route} from 'react-router-dom'
+import About from './components/About';
+import Home from './components/Home';
 function App() {
   return (
-   <></>
+   <>
+    <div className="nav">
+      <li><Link className='links' to="/">Home</Link></li>
+      <li><Link className='links' to="/trending">Trending</Link></li>
+      <li><Link className='links' to="/about">About</Link></li>
+      <li><Link className='links' to="/contact">Contact</Link></li>
+    </div>
+
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/trending' element={<center><h1>Trending</h1></center>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<center><h1>Contact</h1></center>} />
+      </Routes>
+   </>
   );
 }
 
