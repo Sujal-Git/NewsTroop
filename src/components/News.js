519dac8c2bdf0c6fee04196a2f5871d0aa563833
Sujal <sujalvishwakarma93@gmail.com>
2023-07-24 09:52:15 +0530

    Added Image Section In News Comp.

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './News.css'
+import LOGO from './prev.png'
 
 export default function News() {
   
   return (
       <>
       <div className="main">
+        <img  src={LOGO}></img>
         <h3>This Is Headline</h3>
         <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong></p>
+        
       </div>
       </>
   )
