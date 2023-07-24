1007303b37a1410581362934df40be1ff5a54bee
Sujal <sujalvishwakarma93@gmail.com>
2023-07-24 17:40:04 +0530

    Added Props for Title and Description

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './News.css'
 import LOGO from './prev.png'
 
-export default function News() {
+export default function News(props) {
   
   return (
       <>
       <div className="main">
         <img  src={LOGO}></img>
-        <h3>This Is Headline</h3>
-        <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong></p>
+        <h3>{props.title}</h3>
+        <p><strong>{props.description}</strong></p>
         
       </div>
       </>
@@ -17,3 +17,7 @@ export default function News() {
 }
 
 // a5af9b32319a49d487b845235138babd
+News.defaultProps={
+  title:"Some Headline",
+  description: "This is the news article about something interesting.",
+}
