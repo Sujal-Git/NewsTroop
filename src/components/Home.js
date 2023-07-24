c5b9fe8a7bb9352a254c471ed523147aeb93190b
Sujal <sujalvishwakarma93@gmail.com>
2023-07-24 10:03:01 +0530

    Added News Structure in Grid Format

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,13 @@ export default function Home() {
     return(
         <>
         <h1>Home Page</h1>
+        <div className="news-container">
         <News/>
         <News/>
         <News/>
         <News/>
         <News/>
+        </div>
 
         </>
     )
