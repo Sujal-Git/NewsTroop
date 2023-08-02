eeff852b93c9ce47dc5fe5f3b484aff7601ae293
Sujal <sujalvishwakarma93@gmail.com>
2023-08-02 19:29:46 +0530

    Using UeEffect to fetch all the articles from page

--- src/components/Home.js
+++ src/components/Home.js
@@ -175,9 +175,23 @@ export default function Home() {
       "content": "My friend Julie was on vacation in Rome with her 14-year-old daughter. During their visit, they stopped to take some selfies. Spotting their struggle to fit themselves and the grandeur of the backdro… [+6533 chars]"
     }
   ]
- const [data, setData] = useState(article)
+ const [data, setData] = useState([])
+ const [page,setPage]=useState(1)
+
+ 
+ useEffect(()=>{
+  const getData=async()=>{
+    let response=await fetch('https://newsapi.org/v2/everything?q=trending&from=2023-07-23&sortBy=popularity&apiKey=a5af9b32319a49d487b845235138babd&page=1')
+     let result=await response.json()
+     console.log(result)
+     setData(result.articles)
+   }
+   getData()
+   
+ },[])
   return (
     <>
+    {console.log('outside hook')}
       <h1>NewsTroop</h1>
       <div className="news-container">
         {/* mapping the articles and rendering it in news component */}
